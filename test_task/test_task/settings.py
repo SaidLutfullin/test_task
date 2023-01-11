01868a407ad8a8be3583db343a9fba017a8dcb5f
Dude <>
2023-01-11 16:01:16 +0500

    static files settings added

--- test_task/test_task/settings.py
+++ test_task/test_task/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = SECRET_KEY
 TELEGRAM_BOT_TOKEN = TELEGRAM_BOT_TOKEN
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- test_task/test_task/urls.py
+++ test_task/test_task/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.authentication.urls')),
     path('', include('applications.telegram_interaction.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
